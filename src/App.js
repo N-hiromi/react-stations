7aba526e59397b565263af2fa6c06b7314f44834
大竹寛美 <ootakehiromi@ootakehirominoMacBook-Pro.local>
2023-01-01 13:05:05 +0900

    station4

--- src/App.js
+++ src/App.js
@@ -8,14 +8,15 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = React.useState("https://images.dog.ceo/breeds/terrier-norwich/n02094258_2380.jpg")
   return (
     <div>
       <header>ヘッダーですよ</header>
-    <div>
-      <h2>Dogアプリ</h2>
-      <p>犬の画像を表示するサイトです</p>
-      <img src="https://images.dog.ceo/breeds/terrier-norwich/n02094258_2380.jpg" alt="犬の写真は外部サイトDogAPIを利用"></img>
-    </div>
+      <div>
+        <h2>Dogアプリ</h2>
+        <p>犬の画像を表示するサイトです</p>
+        <img src= {dogUrl} alt="犬の写真は外部サイトDogAPIを利用"></img>
+      </div>
     </div>
     
   )
