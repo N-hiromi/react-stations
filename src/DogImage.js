a00c7a53d35b68dc59a6d1999230c447caa0eacc
大竹寛美 <ootakehiromi@ootakehirominoMacBook-Pro.local>
2023-01-02 19:48:18 +0900

    station8

--- src/DogImage.js
+++ src/DogImage.js
@@ -2,6 +2,6 @@ import * as React from 'react'
 
 export const DogImage = (props) => {
 	return (
-		<img src= {props.url} alt="犬の写真は外部サイトDogAPIを利用"></img>
+		<img src={props.url} alt="犬の写真は外部サイトDogAPIを利用"></img>
 	)
 }
