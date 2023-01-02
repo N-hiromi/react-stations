e30270e4c893d35986048e3ef7988b5485f3c79a
大竹寛美 <ootakehiromi@ootakehirominoMacBook-Pro.local>
2023-01-02 19:03:26 +0900

    station8

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@
 
 import * as React from 'react'
 import './App.css'
-import {Header} from 'src/Header'
-import {Description} from 'src/Description'
+import {Header} from './src/Header'
+import {Description} from './src/Description'
+import App from '../src/App'
 
 /**
  * 
  * @type {React.FC}
  */
 export const App = () => {
-  
-
   return (
     <div>
       <Header title={'ヘッダーですよ'}></Header>
