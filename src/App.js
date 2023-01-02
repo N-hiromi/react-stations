2031f918517e2197acde5e894c284d0b16f68ff5
大竹寛美 <ootakehiromi@ootakehirominoMacBook-Pro.local>
2023-01-02 20:30:09 +0900

    station9

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import * as React from 'react'
 import './App.css'
 import { Header } from './Header'
 import { Description } from './Description'
-
+import { DogListContainer } from './DogListContainer'
 /**
  * 
  * @type {React.FC}
@@ -14,6 +14,7 @@ export const App = () => {
     <div>
       <Header />
       <Description />
+      <DogListContainer />
     </div>
   )
 }
--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -1,1 +1,31 @@
 // DO NOT DELETE
+
+import * as React from 'react'
+
+export const DogListContainer = (props) => {
+	const [breeds, setBreeds] = React.useState();
+	React.useEffect(() => {
+		const getDogList = async () => {
+			const res = await fetch("https://dog.ceo/api/breeds/list/all");
+			const json = await res.json();
+			const list = await Object.keys(json.message);
+			console.log(list);
+			setBreeds(list);
+			
+			//listを繰り返して表示
+			// const roop = () => {
+			// 	const items = [];
+			// 	for (let i = 0; i < list[i]; i++){
+			// 		items.push(<li>{ list[i] }</li>)
+			// 	}
+			// 	return <ul>{ items }</ul>
+			// }
+		}
+		getDogList();
+		
+
+	}, [])
+	return (
+			<p>{ breeds }</p>
+	)
+}
