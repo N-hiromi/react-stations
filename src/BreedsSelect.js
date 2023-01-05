41b53fd7239e80d67ca3759167e36c05856754c0
大竹寛美 <ootakehiromi@ootakehirominoMacBook-Pro.local>
2023-01-05 16:46:25 +0900

    station10

--- src/BreedsSelect.js
+++ src/BreedsSelect.js
@@ -3,19 +3,25 @@
 import * as React from 'react'
 
 export const BreedsSelect = (props) => {
+	//selectタグの生成、更新
 	const items = [];
-	
 	for (let i in props.breeds){
 		items.push(<option key={ i } value={ props.breeds[i] }>{ props.breeds[i] }</option>)
 	}
-
+	//selectが変更したら発火
 	const changeBreedsState = (e) => {
 		console.log(document.getElementById("breedsList").value);
 		document.getElementById("breedsList").value = e.target.value;
 		props.setSelectedBreed(e.target.value);
 	}
-	
+	//selectの初期値
+	React.useEffect(() => {
+		console.log(document.getElementById("breedsList"));
+		console.log(props.breeds);
+		document.getElementById("breedsList").value = props.breeds[0];
+	}, [])
+	// console.log(document.getElementById("breedsList").value);
 	return (
-		<select id="breedsList" onChange={ changeBreedsState }>{ items }</select>
+		<select id="breedsList" onChange={ changeBreedsState } >{ items }</select>
 	)
 }
--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -22,7 +22,7 @@ export const DogListContainer = () => {
 	// 	const res = await fetch("https://dog.ceo/api/breed/" + selectedBreed + "/images");
 	// 	const json = await res.json();
 	// 	const imageList = json.message;
-	// 	//取得件数を12件に制限。101件目から削除して格納する
+	// 	//取得件数を12件に制限。13件目から削除して格納する
 	// 	const regImageList = imageList.filter((img, index) => {
 	// 		return index < 12;
 	// 	}) 
